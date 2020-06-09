d498fa355f9ef802b92a15db2b03deef9b8f877e
Vanessa Guerrero <vamarilug@gmail.com>
2020-06-09 14:54:24 -0700

    Add additional spec returning boolean true if divisible by 400
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,7 @@ $(document).ready(function() {
 });
 
 function leapYear(year) {
-  return false;
-}
-
-function leapYear(year) {
-  if ((year % 100 !== 0) && (year % 4 === 0)) {
+  if ((year % 100 !== 0) && (year % 4 === 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
